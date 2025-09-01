359cef4f4ef06bce385e602c7d21c0c381e0d0b3
Nadishan Chathuranga <nadishanchathuranga21@gmail.com>
2025-09-01 18:25:36 +0530

    owner name change

--- database/seeders/OwnerSeeder.php
+++ database/seeders/OwnerSeeder.php
@@ -13,10 +13,10 @@ class OwnerSeeder extends Seeder
     {
         // Owners list
         $ownersData = [
-            ['name' => 'John Doe',      'code' => 'OWN-001'],
-            ['name' => 'Jane Smith',    'code' => 'OWN-002'],
-            ['name' => 'Michael Lee',   'code' => 'OWN-003'],
-            ['name' => 'Sarah Connor',  'code' => 'OWN-004'],
+            ['name' => 'Ayodhya Wijethunga',      'code' => 'OWN-001'],
+            ['name' => 'Kasun Dissanayaka',    'code' => 'OWN-002'],
+            ['name' => 'Shashika Meegoda',   'code' => 'OWN-003'],
+            ['name' => 'Buddika Katudeniya',  'code' => 'OWN-004'],
         ];
 
         // Insert or update owners by code
